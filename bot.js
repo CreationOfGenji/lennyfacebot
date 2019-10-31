f24947d375c87e19737a79336746853318b95cca
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2019-10-31 22:08:50 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,13 @@
-require('http').createServer().listen(3000)
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const http = require('http');
+http.createServer((req, res) => {
+res.writeHead(200, {
+    'Content-type': 'text/plain'
+});
+    res.write('Hey');
+    res.end();
+}).listen(4000);
  
 client.on("ready", () => {
   console.log("I am ready!");
