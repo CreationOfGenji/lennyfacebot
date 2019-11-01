4fb230ab77dda0131f99cdecab8d4e97e73edf87
CreationOfGenji <36540336+CreationOfGenji@users.noreply.github.com>
2019-11-01 17:09:06 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const fs = require("fs");
  
 bot.on("ready", () => {
   console.log("I am ready!");
@@ -8,8 +9,38 @@ bot.on("ready", () => {
 bot.on("message", (message) => {
   let prefix = "l-"
   if (message.content.startsWith(`${prefix}ping`)) {
-    message.channel.send(bot.ping[0]);
+    message.channel.send(`BOO! Did i scare you? The bot's current ping is ${bot.ping}ms`);
+  }
+
+  if(message.content.startsWith(`${prefix}lennytime`)){
+    let folder = "./trainer"
+    message.channel.send("Welcome to ze lenny bot! what lenny do you want to pick!\n1. ( ͡° ͜ʖ ͡°)\n2. ( ° ͜ʖ °)\ntype the number of lenny you want! you have 15 seconds to reply! (aka 1, 2 ect)")
+    const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, { time: 15000 });
+    collector.on("collect", message => {
+      if(message.content === "1"){
+       let lenny = {
+    lenny: "( ͡° ͜ʖ ͡°)"
+    }
+    let json = JSON.stringify(lenny)
+    fs.writeFileSync(`${folder}/${message.author.id}.json`, json)
+    message.channel.send("done! hopefully")
+    collector.stop();  
+      } else if(message.content === "2") {
+        let lenny = {
+          lenny: "( ° ͜ʖ °)"
+          }
+          let json = JSON.stringify(lenny)
+          fs.writeFileSync(`${folder}/${message.author.id}.json`, json)
+          message.channel.send("done! hopefully")
+          collector.stop();         
+      }
+    })
+    collector.on("end", (collected,reason) => {
+      if(reason == "time"){
+        message.reply(` you failed to reply in time!`)
+      }
+    })
   }
 });
  
-client.login(process.env.token);
+bot.login(process.env.token);
